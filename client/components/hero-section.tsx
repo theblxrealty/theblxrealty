51fd7a4a5c4037fb407367beeb50a8bd5974e15c
preeeetham <spreethamkumar5@gmail.com>
2025-07-31 19:54:15 +0530

    feat: update hero section underline animations to red color theme

--- client/components/hero-section.tsx
+++ client/components/hero-section.tsx
@@ -95,7 +95,7 @@ export default function HeroSection() {
                   >
                     <span className="relative">
                       Buy Properties
-                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-500 transition-all duration-300 group-hover:w-full"></span>
                     </span>
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
@@ -108,7 +108,7 @@ export default function HeroSection() {
                   >
                     <span className="relative">
                       Sell Properties
-                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+                      <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-500 transition-all duration-300 group-hover:w-full"></span>
                     </span>
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
